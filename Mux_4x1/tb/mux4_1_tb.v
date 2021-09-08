88f30f52fcdb072f4510a4d0a9c1a7e9404a0ce8
Saaswath <54203063+infini8-13@users.noreply.github.com>
2021-09-08 19:51:31 +0530

    Update mux4_1_tb.v

--- Mux_4x1/tb/mux4_1_tb.v
+++ Mux_4x1/tb/mux4_1_tb.v
@@ -1,24 +1,22 @@
 module mux4_1_tb();
 
-// Declaration of the variables required for testbench
+// Variables required for testbench
   reg [1:0]sel;
   reg [3:0]a;
   wire y;
 
-// Declaration of internal variables required for testbench 
   integer i, j;
 
-// Step 1. Instantiate the Design 
+// Instantiating the design 
   mux4_1 dut (a, sel, y);
 
-// Step 2. Define body for the initialize task to initialize inputs of DUT to 0 
+
   task initialize;
    begin 
     {a,sel} = 0;		// body here
    end
   endtask
   
-// Step 3. Declare tasks with arguments for driving stimulus to DUT.
   task inputs (input [3:0] in);
    begin
     a = in;
@@ -30,7 +28,7 @@ module mux4_1_tb();
     sel = s;
    end
   endtask 
-// Step 4. Call the tasks from procedural block 
+//  Calling the tasks from procedural block 
   initial
    begin
     initialize;
@@ -46,11 +44,9 @@ module mux4_1_tb();
    end
     
 
-// Step 5. Use $monitor task in a parallel initial block  to display inputs and outputs.
   initial $monitor("Input a=%b, select=%b, Output y=%b\n",
                     a, sel, y);
 
-// Step 6. Use $finish task to finish the simulation in a parallel initial block with appropriate delay.
   initial #700 $finish;
 
 endmodule
