59cd70e0f564bb76c46745236ba342db7f0c159d
Saaswath <54203063+infini8-13@users.noreply.github.com>
2021-09-08 20:02:58 +0530

    Comments added

--- Single Port RAM/rtl/ram.v
+++ Single Port RAM/rtl/ram.v
@@ -7,16 +7,17 @@ module ram(data,
         enable;
   input [3:0] addr;
   inout [7:0] data;
-// Step 1. Declare a 8 bit wide memory having 16 locations.
+           
+// 8-bit wide memory with 16 locations
   reg [7:0] mem [15:0];
 
 
-// Step 2. Understand the logic for writing data into a memory location 
+// Writing data into memory
   always@(data,we,enable,addr)
     if (we && !enable)
        mem[addr]=data;
 
-// Step 3. Understand the logic of reading data from a memory location 
+// Reading data from memory
   assign data= (enable && !we) ? mem[addr] : 8'hzz;
 
 endmodule 
